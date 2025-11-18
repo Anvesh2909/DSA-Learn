62bb72ccfa0d0be0a97f5b6ed57f3e01d9a307f7
Anvesh Yadav <anveshyadavmokam@gmail.com>
2025-11-18 21:02:54 +0530

    day-19

---
+++ src/DSA/DynamicProgramming/Medium/CoinChangeTwo.java
@@ -0,0 +1,14 @@
+package DSA.DynamicProgramming.Medium;
+
+public class CoinChangeTwo {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int coin:coins){
+            for(int i=coin; i<=amount; i++){
+                dp[i]+=dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
